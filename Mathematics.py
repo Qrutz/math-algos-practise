8255bdeeb130d31cca94673d35f763799dd30695
Qrutz <Johan-Sandgren@hotmail.com>
2021-01-01 18:31:03 +0100

    x

--- Mathematics.py
+++ Mathematics.py
@@ -1,1 +1,24 @@
-ECHO is on.
+def printDivisors(n):
+    x = []
+    i = 1
+    while i <= n:
+        if (n % i == 0):
+            x.append(i)
+        i = i + 1
+    return x
+
+
+x = printDivisors(12)
+
+largest = [0]
+# for i in x:
+#     if [i] > largest:
+#         largest = [i]
+
+
+# print(largest)
+for i in x:
+    for j in i:
+        if ([i]+[j]) > largest:
+            largest = ([i]+[j])
+print(largest)
