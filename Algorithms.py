2cf6512de801ea0db98bb3e6546e91ce1a1ed948
Qrutz <Johan-Sandgren@hotmail.com>
2021-01-03 15:13:56 +0100

    o

--- Algorithms.py
+++ Algorithms.py
@@ -1,66 +1,66 @@
-# goal is to find the longest subarray and print the len of that subarray
+# # goal is to find the longest subarray and print the len of that subarray
 
-import numpy as np
-arr = np.array([1, 2, 2, 3, 1, 2])
-inpt = arr.tolist()
+# import numpy as np
+# arr = np.array([1, 2, 2, 3, 1, 2])
+# inpt = arr.tolist()
 
-# get ze subarrays
-
-
-def pickingNumbers(a):
-    base = []
-    subarray = [base]
-    for i in range(len(a)):
-        new = a[i]
-        orig = subarray[:]
-        for j in range(len(subarray)):
-
-            subarray[j] = subarray[j] + [new]
-        subarray = orig + subarray
-    return subarray
-
-# get the longest ones lengt
-# [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+# # get ze subarrays
 
 
-def findLongest(x):
+# def pickingNumbers(a):
+#     base = []
+#     subarray = [base]
+#     for i in range(len(a)):
+#         new = a[i]
+#         orig = subarray[:]
+#         for j in range(len(subarray)):
 
-    # if len of x[i] > len of x[i+1] , answer = x[i]
-    n = 0
-    highest = 0
+#             subarray[j] = subarray[j] + [new]
+#         subarray = orig + subarray
+#     return subarray
 
-    for i in range(n, len(x)-1):
-        print(len(x[i+1]), len(x[i]))
-        if len(x[i]) > highest:
-            highest = len(x[i])
-            print(highest, " this the high")
-            if highest > 0 and len(x[i+1]) > highest:
-                highest = len(x[i+1])
-                return highest
-    #     if len(x[i+1]) > highest:
-    #         highest = len(x[i+1])
-    #         print("swapped vars", highest)
-    # return highest
-    # highest == len(x[i+1])
-
-    # highest = len(x[i+1])
-    # return highest
-    # emptyarr.insert(len[x+1])
-    # return emptyarrS
-    # print(len(x[i+1]), len(x[i]))
-
-    # if len(x[i+1]) > len(x[i]):
-
-    # for j in range(n+1, len(x)):
-    #     print(x[j], "666")
-
-    #  if len(x[j]) >= len(x[i]):
-    #      print(len(x[j]))
-    #      print(len(x[j]), len(x[i]))
+# # get the longest ones lengt
+# # [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
 
 
-y = pickingNumbers([1, 2, 3])
-abba = y[3]
+# def findLongest(x):
+
+#     # if len of x[i] > len of x[i+1] , answer = x[i]
+#     n = 0
+#     highest = 0
+
+#     for i in range(n, len(x)-1):
+#         print(len(x[i+1]), len(x[i]))
+#         if len(x[i]) > highest:
+#             highest = len(x[i])
+#             print(highest, " this the high")
+#             if highest > 0 and len(x[i+1]) > highest:
+#                 highest = len(x[i+1])
+#                 return highest
+#     #     if len(x[i+1]) > highest:
+#     #         highest = len(x[i+1])
+#     #         print("swapped vars", highest)
+#     # return highest
+#     # highest == len(x[i+1])
+
+#     # highest = len(x[i+1])
+#     # return highest
+#     # emptyarr.insert(len[x+1])
+#     # return emptyarrS
+#     # print(len(x[i+1]), len(x[i]))
+
+#     # if len(x[i+1]) > len(x[i]):
+
+#     # for j in range(n+1, len(x)):
+#     #     print(x[j], "666")
+
+#     #  if len(x[j]) >= len(x[i]):
+#     #      print(len(x[j]))
+#     #      print(len(x[j]), len(x[i]))
 
 
-print(findLongest(y))
+# y = pickingNumbers([1, 2, 3])
+# abba = y[3]
+
+
+# print(findLongest(y))
--- Data-Structures.py
+++ Data-Structures.py
@@ -1,1 +1,28 @@
-ECHO is on.
+# Arrays
+# def reverseArray(a):
+#     copy = a.copy()
+#     copy.reverse()
+#     return copy
+
+
+# inpt = ["1", "2", "3", "4"]
+# inpt2 = [3, 2, 3, 4]
+
+# print(reverseArray(inpt))
+# print(reverseArray(inpt2))
+
+# Doing it the hard way-
+
+
+arr = [1, 2, 3, 4, 5]
+print("Original array: ")
+for i in range(0, len(arr)):
+    print(arr[i]),
+print("Array in reverse order: ")
+# Loop through the array in reverse order
+for i in range(len(arr)-1, -1, -1):
+    print(arr[i])
+
+
+for i in range(5, -1, -1):
+    print(i)
