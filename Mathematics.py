73637e32ada23df07b5de835ec5f7d0e09d503df
Qrutz <Johan-Sandgren@hotmail.com>
2021-01-20 13:31:33 +0100

    matrix solver

--- Mathematics.py
+++ Mathematics.py
@@ -1,25 +1,37 @@
-def printDivisors(n):
-    x = []
-    i = 1
-    while i <= n:
-        if (n % i == 0):
-            x.append(i)
-        i = i + 1
-    return x
+# def printDivisors(n):
+#     x = []
+#     i = 1
+#     while i <= n:
+#         if (n % i == 0):
+#             x.append(i)
+#         i = i + 1
+#     return x
 
 
-x = printDivisors(12)
+# x = printDivisors(12)
 
-largest = [0]
-# for i in x:
-#     if [i] > largest:
-#         largest = [i]
+# largest = [0]
+# # for i in x:
+# #     if [i] > largest:
+# #         largest = [i]
 
 
-# print(largest)
-xd = ["Digg", "Nig", "Zigg"]
+# # print(largest)
+# xd = ["Digg", "Nig", "Zigg"]
 
-for i in xd:
-    for j in i:
-        for z in j:
-            print(z)
+# for i in xd:
+#     for j in i:
+#         for z in j:
+#             print(z)
+
+import numpy as np
+
+
+def solveMatrix(A, b):
+    solved = np.linalg.solve(A, b)
+    return solved
+
+
+A = np.array([[1, -2, -1], [2, 2, -1], [-1, -1, 2]])
+b = np.array([1, 2, 1])
+print(solveMatrix(A, b))
